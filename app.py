9dadc21a3a9a86bcc25e0f5067af91611aabe226
unknown <oksanochkacov90@gmail.com>
2021-04-23 05:21:51 +0300

    creating ui for sending request to client-service

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    print("PFPFPFP")
     states = requests.get("http://127.0.0.1:5000/states")
     realty_types = requests.get("http://127.0.0.1:5000/realty_types")
     return render_template("index.html", states=states.json(), realty_types=realty_types.json(), list={})
@@ -42,7 +41,7 @@ def result():
                 except ValueError:
                     return redirect(url_for('main_page'))
     response_from_server = requests.post("http://127.0.0.1:5000/realty", json=request_to_client_service).json()
-    print(response_from_server['response'])
+    print(response_from_server)
     return render_template('results.html', list=response_from_server['response'])
 
 app.run(host='127.0.0.1', port=8000)
