19dc1c4d2d7ff70a70caa10546b403d199074229
unknown <oksanochkacov90@gmail.com>
2021-04-23 04:54:46 +0300

    basic ui with state and type filters

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def main_page():
+    states = requests.get("http://127.0.0.1:5000/states")
+    realty_types = requests.get("http://127.0.0.1:5000/realty_types")
+    return render_template("index.html", states=states.json(), realty_types=realty_types.json())
 
 
-app.run(host='127.0.0.1', port=8080)
+@app.route('/result')
+def result():
+    operation_types = requests.get("http://127.0.0.1:5000/operation_types")
+    operation_types_list = operation_types.json()
+    response = request.args
+    print(response)
+    return "HELLO"
+
+app.run(host='127.0.0.1', port=8000)
